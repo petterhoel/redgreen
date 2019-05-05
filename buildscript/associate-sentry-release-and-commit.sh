6ea2b0f788bf093e5bcb0faef323ea4d78b8b724
Petter Hoel <1263775+petterhoel@users.noreply.github.com>
2019-05-05 14:01:27 +0200

    editd buildscript

--- buildscript/associate-sentry-release-and-commit.sh
+++ buildscript/associate-sentry-release-and-commit.sh
@@ -1,15 +1,13 @@
 #  Assumes we're in a git repository and sentry cli is installed
-INSTALL_DIR=$(pwd)
-INSTALL_PATH="${INSTALL_DIR}/sentry-cli"
-echo $INSTALL_PATH
-echo $INSTALL_DIR
+export INSTALL_DIR=$(pwd)
+export INSTALL_PATH="${INSTALL_DIR}/sentry-cli"
 
 curl -sL https://sentry.io/get-cli
 
 export SENTRY_AUTH_TOKEN=$SENTRY_AUTH
 export SENTRY_ORG="petterhoel"
-$INSTALL_PATH ---version
-sentry-cli --version
+$INSTALL_PATH --version
+#sentry-cli --version
 #VERSION=`sentry-cli releases propose-version`
 
 # Create a release
