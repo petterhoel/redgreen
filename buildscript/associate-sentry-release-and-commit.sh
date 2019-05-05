fb1a2c9352ad16059e6f62f1b459b0069ece1131
Petter Hoel <1263775+petterhoel@users.noreply.github.com>
2019-05-05 12:12:29 +0200

    adds debugging for netlify build

--- buildscript/associate-sentry-release-and-commit.sh
+++ buildscript/associate-sentry-release-and-commit.sh
@@ -1,12 +1,12 @@
 #  Assumes we're in a git repository
-export SENTRY_AUTH_TOKEN=$SENTRY_AUTH
-export SENTRY_ORG="petterhoel"
+#export SENTRY_AUTH_TOKEN=$SENTRY_AUTH
+#export SENTRY_ORG="petterhoel"
 curl -sL https://sentry.io/get-cli/ | bash
-VERSION=`sentry-cli releases propose-version`
+#VERSION=`sentry-cli releases propose-version`
 
 # Create a release
-sentry-cli releases new -p "buildscreen" "$VERSION"
+#sentry-cli releases new -p "buildscreen" "$VERSION"
 
 # Associate commits with the release
-sentry-cli releases set-commits --auto "$VERSION"
-sentry-cli releases finalize "$VERSION"
+#sentry-cli releases set-commits --auto "$VERSION"
+#sentry-cli releases finalize "$VERSION"
