637a9963d6cdf96d068e53bda064fe665025d8cd
Petter Hoel <1263775+petterhoel@users.noreply.github.com>
2019-05-05 13:50:13 +0200

    ads some discovery debugs

--- buildscript/associate-sentry-release-and-commit.sh
+++ buildscript/associate-sentry-release-and-commit.sh
@@ -1,4 +1,13 @@
 #  Assumes we're in a git repository and sentry cli is installed
+echo "pwd"
+pwd
+
+echo "installpath $INSTALL_PATH"
+
+echo "installdir $INSTALL_DIR"
+
+#curl -sL https://sentry.io/get-cli
+
 export SENTRY_AUTH_TOKEN=$SENTRY_AUTH
 export SENTRY_ORG="petterhoel"
 sentry-cli --version
