3b795da3a4d1fcba867101f4faf0300f8514e001
Petter Hoel <1263775+petterhoel@users.noreply.github.com>
2019-05-05 13:25:24 +0200

    removes sentry cli installation (moved to separate step).

--- buildscript/associate-sentry-release-and-commit.sh
+++ buildscript/associate-sentry-release-and-commit.sh
@@ -1,13 +1,12 @@
-#  Assumes we're in a git repository
-#export SENTRY_AUTH_TOKEN=$SENTRY_AUTH
-#export SENTRY_ORG="petterhoel"
-curl -sL https://sentry.io/get-cli/
+#  Assumes we're in a git repository and sentry cli is installed
+export SENTRY_AUTH_TOKEN=$SENTRY_AUTH
+export SENTRY_ORG="petterhoel"
 sentry-cli --version
-#VERSION=`sentry-cli releases propose-version`
+VERSION=`sentry-cli releases propose-version`
 
 # Create a release
-#sentry-cli releases new -p "buildscreen" "$VERSION"
+sentry-cli releases new -p "buildscreen" "$VERSION"
 
 # Associate commits with the release
-#sentry-cli releases set-commits --auto "$VERSION"
-#sentry-cli releases finalize "$VERSION"
+sentry-cli releases set-commits --auto "$VERSION"
+sentry-cli releases finalize "$VERSION"
