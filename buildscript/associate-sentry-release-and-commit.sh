817ed66b371d00115ae5a66da37edd1baf8538ee
Petter Hoel <1263775+petterhoel@users.noreply.github.com>
2019-05-05 13:57:35 +0200

    Adds trix

--- buildscript/associate-sentry-release-and-commit.sh
+++ buildscript/associate-sentry-release-and-commit.sh
@@ -1,21 +1,20 @@
 #  Assumes we're in a git repository and sentry cli is installed
-echo "pwd"
-pwd
+INSTALL_DIR=$(pwd)
+INSTALL_PATH="${INSTALL_DIR}/sentry-cli"
+echo $INSTALL_PATH
+echo $INSTALL_DIR
 
-echo "installpath $INSTALL_PATH"
-
-echo "installdir $INSTALL_DIR"
-
-#curl -sL https://sentry.io/get-cli
+curl -sL https://sentry.io/get-cli
 
 export SENTRY_AUTH_TOKEN=$SENTRY_AUTH
 export SENTRY_ORG="petterhoel"
+$INSTALL_PATH ---version
 sentry-cli --version
-VERSION=`sentry-cli releases propose-version`
+#VERSION=`sentry-cli releases propose-version`
 
 # Create a release
-sentry-cli releases new -p "buildscreen" "$VERSION"
+#sentry-cli releases new -p "buildscreen" "$VERSION"
 
 # Associate commits with the release
-sentry-cli releases set-commits --auto "$VERSION"
-sentry-cli releases finalize "$VERSION"
+#sentry-cli releases set-commits --auto "$VERSION"
+#sentry-cli releases finalize "$VERSION"
