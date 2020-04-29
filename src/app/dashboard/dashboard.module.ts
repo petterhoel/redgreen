59238017836ef32c66ffbde424fc915b65c00120
Petter <1263775+petterhoel@users.noreply.github.com>
2020-04-29 18:54:39 +0200

    Swap moment  for date-fns. Only login should be vertically centered. (#16)

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LatestBuildsComponent } from './latest-builds/latest-builds.component';
 import { BuildInfoComponent } from './build-info/build-info.component';
 import { RouterModule } from '@angular/router';
-import { MomentModule } from 'ngx-moment';
 import { BranchPipe } from './branch.pipe';
 import { SettingsComponent } from './settings/settings.component';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { DateFnsModule } from 'ngx-date-fns';
 
 
 @NgModule({
@@ -20,7 +20,7 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     RouterModule,
-    MomentModule,
+    DateFnsModule.forRoot(),
     FormsModule,
   SharedModule,
   ],
