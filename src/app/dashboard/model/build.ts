bfbb531f4af1c7197129cfa11296cee7a3398e33
Petter <petter@Lottas-MacBook-Pro.local>
2020-05-01 16:38:08 +0200

    Fixes indicator for never builts

--- src/app/dashboard/model/build.ts
+++ src/app/dashboard/model/build.ts
@@ -1,7 +1,7 @@
 import { LastChanges } from './last-changes';
 export class Build {
   number: string = '';
-  status: 'FAILURE' | 'SUCCESS' = 'FAILURE';
+  status: 'FAILURE' | 'SUCCESS' | '' = '';
   statusText: string  = '';
   lastChanges: LastChanges = { change: [] };
   branchName: string  = '';
