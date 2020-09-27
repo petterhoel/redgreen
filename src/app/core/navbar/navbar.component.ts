dece018204c5d83928ecc41a2ba05e96832e515d
Petter <petter@petterhoel.net>
2020-09-27 17:20:06 +0200

    started menu

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent { }
+export class NavbarComponent implements OnInit{
+  private readonly MOBILE_BREAKPOINT = 768;
+  private readonly menuState = new BehaviorSubject<MenuVisibility>({
+    layoutDirection: 'column',
+    menuVisible: false
+  });
+  menuState$ = this.menuState.asObservable();
+  @HostListener('window:resize', ['$event']) onResize(event: Event) {
+    if (event.target) {
+      this.adjustMenu();
+    }
+  }
+
+  private adjustMenu(): void {
+    const largeScreen = window.innerWidth > this.MOBILE_BREAKPOINT;
+    const { menuVisible, layoutDirection  } = this.menuState.value;
+    if (!largeScreen && menuVisible && layoutDirection === `column`) {
+      return
+    }
+    this.menuState.next({
+      menuVisible: largeScreen ,
+      layoutDirection: largeScreen ? `row` : `column`
+    })
+  }
+
+  toggleVisiblility(): void {
+    const { layoutDirection, menuVisible  } = this.menuState.value;
+    if (layoutDirection === `row`) {
+      return;
+    }
+    this.menuState.next({
+      layoutDirection,
+      menuVisible: !menuVisible
+    })
+  }
+
+  ngOnInit(): void {
+    this.adjustMenu();
+  }
+}
+
+interface MenuVisibility {
+  layoutDirection: `column`  | `row`;
+  menuVisible: boolean;
+}
