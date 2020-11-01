ffde5d4378727ec5845acda10aadb697012e1a2e
Petter <petter@petterhoel.net>
2020-11-01 15:53:08 +0100

    removes dummy stuff and adds some truncate pipe tests.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { TruncatePipe } from './pipe/truncate.pipe';
 import { ClipboardModule } from 'ngx-clipboard';
 import { StatusIconComponent } from './status-icon/status-icon.component';
+import { SomePipe } from './pipe/some.pipe';
 
 @NgModule({
-  declarations: [TruncatePipe, StatusIconComponent],
+  declarations: [TruncatePipe, StatusIconComponent, SomePipe],
   imports: [
     CommonModule
   ],
