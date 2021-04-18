f391183a6735490f460bc8da34e61cfce7470412
Petter <1263775+petterhoel@users.noreply.github.com>
2021-04-18 15:44:45 +0200

    rm debug code (#38)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   {
     path: `dashboard`,
     component: LatestBuildsComponent,
-   // canActivate: [ AuthGuardService ]
+    canActivate: [ AuthGuardService ]
   },
   {
     path: `settings`,
